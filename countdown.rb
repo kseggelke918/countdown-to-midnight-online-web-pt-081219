8722828e94ba534558af816cb9fa0f2492c91482
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-13 17:46:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep(number)
+def countdown_with_sleep(number)
   while number >= 0
     puts "#{number} SECONDS(S)!" 
     number -= 1
