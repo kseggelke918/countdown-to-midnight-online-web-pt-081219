32a6524fccc28527fe1e7b148897249c69647648
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-13 17:39:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,6 @@
-#write your code here
-
-
 def countdown(number)
-  while number >= 0
-    puts "#{number} SECONDS(S)!" 
-  end 
-  return "HAPPY NEW YEAR!"
+ # while number >= 0
+  #  puts "#{number} SECONDS(S)!" 
+  #end 
+  #return "HAPPY NEW YEAR!"
 end
